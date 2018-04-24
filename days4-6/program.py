bb8ef6fee5f8534908c805c8ffdc74da3068cfd1
Blake <blakeaa@gmail.com>
2018-04-24 17:08:22 -0500

    PyBites challenge 13 finished!

--- days4-6/program.py
+++ days4-6/program.py
@@ -1,4 +1,7 @@
 import collections
+import math
+import statistics
+
 import requests
 import csv
 
@@ -8,6 +11,9 @@ def main():
     experienced_directors = qualify_directors(movies, 4)
     movies_by_director = sort_movies_by_director(movies, experienced_directors)
     rated_directors = rate_directors(movies_by_director)
+    display_top_directors(rated_directors, movies_by_director)
+    # for director, rating in rated_directors.items():
+    #     print(f"{director}'s avg rating is {rating:.1f}.")
 
 def get_movie_list():
     url = 'https://raw.githubusercontent.com/sundeepblue/' \
@@ -56,14 +62,39 @@ def qualify_directors(movies, required_count):
 def sort_movies_by_director(movies, directors):
     sorted_movies = dict()
     for director in directors:
-        sorted_movies[director] = [movie
-                                    for movie in movies
-                                    if movie['director_name'] == director]
+        sorted_movies[director] = sorted([
+            movie
+            for movie in movies
+            if movie['director_name'] == director],
+            key=lambda movie: float(movie['imdb_score']), reverse=True)
     return sorted_movies
 
 
 def rate_directors(movies_by_director):
-    pass
+    rated_directors = collections.OrderedDict()
+    for director, movies in movies_by_director.items():
+        rated_directors[director] = statistics.mean(
+            [float(x['imdb_score']) for x in movies]
+        )
+    rated_directors = collections.OrderedDict(sorted(
+        rated_directors.items(), key=lambda director: director[1],
+        reverse=True
+    ))
+    return rated_directors
+
+
+def display_top_directors(rated_directors, movies_by_director, top=20):
+    top_directors = [director for i, director
+                     in enumerate(rated_directors.keys())
+                     if i < top]
+    for i, director in enumerate(top_directors):
+        print(f"{i+1:2.0f}. {director}:     "
+              f"average rating = {rated_directors[director]:1.1f}")
+        print('------------------------------------------------------')
+        for movie in movies_by_director[director]:
+            print(f"    {movie['title_year']}] {movie['movie_title']}: "
+                  f"{movie['imdb_score']}")
+        print()
 
 
 if __name__ == '__main__':
