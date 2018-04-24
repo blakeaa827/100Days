a07570954286e399c13044377d73b7996c8b1ae7
Blake <blakeaa@gmail.com>
2018-04-24 09:34:42 -0500

    PyBites challenge 13 - progress

--- days4-6/program.py
+++ days4-6/program.py
@@ -5,7 +5,9 @@ import csv
 
 def main():
     movies = get_movie_list()
-    qualify_directors(movies)
+    experienced_directors = qualify_directors(movies, 4)
+    movies_by_director = sort_movies_by_director(movies, experienced_directors)
+    rated_directors = rate_directors(movies_by_director)
 
 
 def get_movie_list():
@@ -13,19 +15,56 @@ def get_movie_list():
           'movie_rating_prediction/master/movie_metadata.csv'
     r = requests.get(url)
     movies = list(csv.DictReader(r.content.decode('utf-8').splitlines()))
+    processed_movies = list()
     for index, movie in enumerate(movies):
-        movies[index] = collections.OrderedDict([
-            ('director_name', movie['director_name']),
-            ('movie_title', movie['movie_title']),
-            ('title_year', movie['title_year']),
-            ('imdb_score', movie['imdb_score']),
-        ])
-    return movies
+        for key, value in movie.items():
+            movie[key] = value.replace(u'\xa0', '')
+            movie[key] = value.strip()
+
+        try:
+            if int(movie['title_year']) < 1960:
+                print(f"Skipping {movie['movie_title']} - "
+                      f"{movie['title_year']}")
+                continue
+        except ValueError:
+            # print(f"Skipping {movie['movie_title']}. "
+            #       f"Unknown year.")
+            # break
+            pass
+
+        if not movie['director_name']:
+            print(f"Skipping {movie['movie_title']}. "
+                  f"Unknown director.")
+            continue
+
+        processed_movies.append(collections.OrderedDict([
+            ('director_name', movie['director_name'].strip()),
+            ('movie_title', movie['movie_title'].rstrip(u'\xa0').strip()),
+            ('title_year', movie['title_year'].strip()),
+            ('imdb_score', movie['imdb_score'].strip()),
+        ]))
+    return processed_movies
 
 
-def qualify_directors(movies):
+def qualify_directors(movies, required_count):
     movie_count = collections.Counter([x['director_name'] for x in movies])
-    print(movie_count.most_common(5))
+    directors = [director
+                 for director, count in movie_count.items()
+                 if count >= required_count]
+    return directors
+
+
+def sort_movies_by_director(movies, directors):
+    sorted_movies = collections.defaultdict(list)
+    for director in directors:
+        sorted_movies[director] += [movie
+                                    for movie in movies
+                                    if movie['director_name'] == director]
+    return sorted_movies
+
+
+def rate_directors(movies_by_director):
+    pass
 
 
 if __name__ == '__main__':
